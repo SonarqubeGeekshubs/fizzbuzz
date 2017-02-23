3f1ac3b37501a70148271520dac5dc5fe33a4a88
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-02-23 16:27:26 +0100

    Project creation

---
+++ src/main/java/org/sonarqube/geekshubs/FizzBuzz.java
@@ -0,0 +1,14 @@
+package org.sonarqube.geekshubs;
+
+/**
+ * Created by ricardo on 23/02/17.
+ */
+public class FizzBuzz {
+  public static String getResult(int number) {
+    if (number % 15 == 0) return "fizzbuzz";
+    else if (number % 3 == 0) return "fizz";
+    else if (number % 5 == 0) return "buzz";
+    return Integer.toString(number);
+  }
+
+}
