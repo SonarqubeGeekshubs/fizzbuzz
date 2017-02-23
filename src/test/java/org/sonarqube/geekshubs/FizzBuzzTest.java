009b6e1fecf927e30e4975a1bf8fb2aaaf5b69b9
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-02-23 19:29:33 +0100

    Change test method names

--- src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
+++ src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
@@ -12,24 +12,24 @@ public class FizzBuzzTest {
   private FizzBuzz fizzBuzz = new FizzBuzz();
 
   @Test
-  public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
+  public void testResultShouldReturnFizzIfTheNumberIsDividableByThree() {
     assertEquals("fizz", fizzBuzz.getResult(3));
   }
 
   @Test
-  public void getResultShouldReturnBuzzIfTheNumberIsDividableBy5() {
+  public void testResultShouldReturnBuzzIfTheNumberIsDividableByFive() {
     assertEquals("buzz", fizzBuzz.getResult(5));
     assertEquals("buzz", fizzBuzz.getResult(10));
   }
 
   @Test
-  public void getResultShouldReturnBuzzIfTheNumberIsDividableBy15() {
+  public void testResultShouldReturnBuzz() {
     assertEquals("fizzbuzz", fizzBuzz.getResult(15));
     assertEquals("fizzbuzz", fizzBuzz.getResult(30));
   }
 
   @Test
-  public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
+  public void testResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
     assertEquals("1", fizzBuzz.getResult(1));
     assertEquals("2", fizzBuzz.getResult(2));
     assertEquals("4", fizzBuzz.getResult(4));
