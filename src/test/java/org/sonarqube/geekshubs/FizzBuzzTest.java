02da912cbdcfb7601e54cdcfd903fa8f7c28ead2
Ricardo García Fernández <ricardogarfe@gmail.com>
2022-01-21 18:51:18 +0100

    feat: Add fizz test scenario

--- src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
+++ src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
@@ -1,5 +1,13 @@
 package org.sonarqube.geekshubs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
+        Assert.assertEquals("fizz", FizzBuzz.getResult(3));
+    }
 
 }
