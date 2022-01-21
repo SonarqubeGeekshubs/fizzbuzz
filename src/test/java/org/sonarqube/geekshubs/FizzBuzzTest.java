c13afd24641616ecc2eebd5a9fe726d5bc3183d9
Ricardo García Fernández <ricardogarfe@gmail.com>
2022-01-21 18:57:34 +0100

    feat: Add fizzbuzz, buzz and number test scenarios

--- src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
+++ src/test/java/org/sonarqube/geekshubs/FizzBuzzTest.java
@@ -1,13 +1,32 @@
 package org.sonarqube.geekshubs;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
     @Test
     public void getResultShouldReturnFizzIfTheNumberIsDividableBy3() {
-        Assert.assertEquals("fizz", FizzBuzz.getResult(3));
+        assertEquals("fizz", FizzBuzz.getResult(3));
+    }
+
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy5() {
+        assertEquals("buzz", FizzBuzz.getResult(5));
+        assertEquals("buzz", FizzBuzz.getResult(10));
+    }
+
+    @Test
+    public void getResultShouldReturnBuzzIfTheNumberIsDividableBy15() {
+        assertEquals("fizzbuzz", FizzBuzz.getResult(15));
+        assertEquals("fizzbuzz", FizzBuzz.getResult(30));
+    }
+
+    @Test
+    public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
+        assertEquals("1", FizzBuzz.getResult(1));
+        assertEquals("2", FizzBuzz.getResult(2));
+        assertEquals("4", FizzBuzz.getResult(4));
     }
 
 }
